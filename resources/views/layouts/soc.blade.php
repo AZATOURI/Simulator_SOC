4f67109869863cd665323c0d58d9c968b7e3c823
useman404 <azatourirabya2017@gmail.com>
2026-05-13 22:26:38 +0100

    update views/routes

--- resources/views/layouts/soc.blade.php
+++ resources/views/layouts/soc.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <title>Agadir SOC</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}">
 
     <style>
         * { box-sizing: border-box; }
